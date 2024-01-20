cab0514416f050e12eb9669a25faff906f8d89ea
Daiado <daiado30@hotmail.com>
2024-01-20 15:46:00 +0100

    initial structure with a multi module gradle project and dockers

---
+++ order/src/main/java/com/cafeteria/order/LoginController.java
@@ -0,0 +1,18 @@
+package com.cafeteria.order;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class LoginController {
+
+
+
+    @GetMapping("/login")
+    public String showLoginPage(ModelMap model){
+        return "welcome";
+    }
+
+}
---
+++ order/src/main/java/com/cafeteria/order/OrderApplication.java
@@ -0,0 +1,19 @@
+package com.cafeteria.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+
+
+}
+
